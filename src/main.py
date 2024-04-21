ea43bf432736aecdf847bd878cb476bf3d0570ef
Rodabaugh <erik@erikrodabaugh.com>
2024-04-20 19:34:10 -0700

    Cleaning up mess I made in main.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from textnode import TextNode
-from htmlnode import *
 
 def main():
     test_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
@@ -7,22 +6,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-def text_node_to_html_node(text_node):
-    support_types = ["text", "bold", "italic", "code", "link", "image"]
-
-    if text_node.type not in support_types:
-        raise Exception("Unsupported Type")
-    
-    if text_node.type == "text":
-        return LeafNode(None, text_node.text)
-    elif text_node.type == "bold":
-        return LeafNode("b", text_node.text)
-    elif text_node.type == "italic":
-        HTMLNode("i", text_node.text)
-    elif text_node.type == "code":
-        HTMLNode("code", text_node.text)
-    elif text_node.type == "link":
-        HTMLNode("a", text_node.text, None, f'href={text_node.url}')
-    elif text_node.type == "image":
-        HTMLNode("img", "", None, f'src="{text_node.url}" alt="{text_node.text}"')
